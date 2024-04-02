8e317ea38b4404bc61a5bc9e304df505f18aa60f
mdmohid <alammohid855@gmail.com>
2024-04-02 14:59:55 +0545

    Diamondshape

---
+++ DiamondShape.java
@@ -0,0 +1,32 @@
+public class DiamondShape {
+    public static void main(String[] args) {
+        System.out.println("   *   ");
+        System.out.println("  ***  ");
+        System.out.println(" ***** ");
+        System.out.println("*******");
+        System.out.println(" ***** ");
+        System.out.println("  ***  ");
+        System.out.println("   *   ");
+    }
+}
+
+/*
+ a. What is the latest version of the Java SDK that is available?
+ -> JDK 21
+
+ b. What is the difference between Java SE and Java ME?
+ -> Java SE (Standard Edition) is for building desktop apps whereas Java ME (Micro Edition) is for old mobile devices.
+
+ c. Which operating system is Java available for?
+ -> Java is available for Linux, macOS, Windows.
+
+ d. What is the most popular IDE available for Java apart from Eclipse?
+ -> IntelliJ IDEA is the most popular IDE available for Java apart from Eclipse.
+
+ e. What is the main() function for in a Java program?
+ -> The Java main method is usually the first method you learn about when you start programming in Java 
+ because its the entry point for executing a Java program. The main method can contain code to execute
+  or call other methods, and it can be placed in any class that's part of a program
+ */
+
+
